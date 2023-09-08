6d6c94c588e0b86f72e5e543d4ca94a4e9aa7c81
Alex Tensorer <alexandre.tensorer@oclock.school>
2023-09-08 14:45:24 +0200

    text modifs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,8 @@ const About = () => {
         </div>
         <p id="l-about-p" className="text-xl mt-15">
         Avant de plonger dans le monde de la tech,
-        j'ai œuvré aux côtés de géants de l'industrie financière,
-        dans le domaine de la gestion de fonds et du patrimoine, tant au Royaume-Uni qu'en France.
+        j'ai travaillé aux côtés de géants de l'industrie financière,
+        dans le domaine de la gestion de fonds d'investissement et de la gestion patrimoine, au Royaume-Uni et en France.
         En 2023, j'ai décidé de me former au développement web avec l'École O'clock.
         J'ai rapidement assimilé de nouveaux concepts et les ai mis en pratique.
         Je porte des idées ambitieuses et j'apporte une attitude positive à chaque défi
